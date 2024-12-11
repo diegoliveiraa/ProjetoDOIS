fe25675c627082fcc3395be956d8250258356482
Diego Oliveira <diegoliveiraa1@gmail.com>
2024-12-11 15:23:55 -0300

    feat: create ScoreRepository

---
+++ projetoDOIS/src/main/java/acc/br/projetoDOIS/repositories/ScoreRepository.java
@@ -0,0 +1,8 @@
+package acc.br.projetoDOIS.repositories;
+
+import acc.br.projetoDOIS.entity.Score;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ScoreRepository extends CrudRepository<Score, Integer> {
+
+}
